a8064403137abb266d5dc7b3f59ad76e56c3dd66
Nattamet <102680145+TonNattamet@users.noreply.github.com>
2025-03-17 01:16:33 +0700

    last final

--- app.py
+++ app.py
@@ -39,12 +39,12 @@ from pages.ml_ThaiFood import show as ml_ThaiFood
 from pages.ml_Bakery_Nutrition import show as ml_Bakery_Nutrition
 from pages.nn_LineTrain_Predict import show as nn_LineTrain_Predict
 
-# Sidebar Information
-# st.sidebar.header("About Us")
-# st.sidebar.markdown("**จัดทำโดย:** นายณัฐเมศร์ วงศ์ธนิตเลิศ (ห้อง DE-RA ชั้นปีที่ 4)")
-# st.sidebar.markdown("**อาจารย์ผู้สอน:** ดร.ณัฐกิตติ์ จิตรเอื้อตระกูล (NJR)")
-# st.sidebar.markdown("**มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ**")
-# st.sidebar.markdown("**ภาควิชาวิทยาการคอมพิวเตอร์และสารสนเทศ**")
+Sidebar Information
+st.sidebar.header("About Us")
+st.sidebar.markdown("**จัดทำโดย:** นายณัฐเมศร์ วงศ์ธนิตเลิศ (ห้อง DE-RA ชั้นปีที่ 4)")
+st.sidebar.markdown("**อาจารย์ผู้สอน:** ดร.ณัฐกิตติ์ จิตรเอื้อตระกูล (NJR)")
+st.sidebar.markdown("**มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ**")
+st.sidebar.markdown("**ภาควิชาวิทยาการคอมพิวเตอร์และสารสนเทศ**")
 
 # Page Navigation
 PAGES = {
